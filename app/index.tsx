dabe8189cd05deea41f9c80dcabf550d322fc2e2
PIO-VIA <piodjiele@gmail.com>
2025-08-07 14:50:20 +0100

    fix the init project

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>bonjour, welcome to the app!</Text>
+      <Text>Enjoy your stay!</Text>
     </View>
   );
 }
