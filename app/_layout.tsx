3dc5bf92c7abd6534e84aff1fb38e4e5f786a09b
PIO-VIA <piodjiele@gmail.com>
2025-08-08 03:43:27 +0100

    add the files for home page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,6 +9,8 @@ export default function RootLayout() {
     }}>
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="login" options={{ headerShown: false }} />
+      <Stack.Screen name="home" options={{ headerShown: false }} />
+
       
     </Stack>
 
